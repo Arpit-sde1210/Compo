a1bfb88c352fc99e765c8597507d5aca881d81dd
Arpit-sde1210 <arpit.sde1210@gmail.com>
2025-11-03 02:02:13 +0530

    Changes Made as Discuused

--- src/compo/menu.js
+++ src/compo/menu.js
@@ -4,10 +4,25 @@ import './menu.css';
 const Menu = () => {
   return (
     <div className="menu">
-      <div className="menu-item">Settings</div>
-      <div className="menu-item">Subscription</div>
+      <div className="menu-container">
+        {/* Settings */}
+        <div className="menu-item">
+          <span>Settings</span>
+        </div>
+        
+        {/* Subscription */}
+        <div className="menu-item">
+          <span>Subscription</span>
+        </div>
+      </div>
+      
+      {/* Horizontal Line */}
       <div className="menu-divider"></div>
-      <div className="menu-item logout">Log out</div>
+      
+      {/* Logout */}
+      <div className="menu-item logout">
+        <span>Log out</span>
+      </div>
     </div>
   );
 };
